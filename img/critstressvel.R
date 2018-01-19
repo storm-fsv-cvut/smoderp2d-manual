762014dd175508eec36f3a5b3f2fc1a81d1f15ce
jerabekjak <jerabekjak@email.cz>
2018-01-19 18:28:53 +0100

    Po druhe revizi s vytistenym manualem.

--- img/critstressvel.R
+++ img/critstressvel.R
@@ -1,23 +1,39 @@
-I    = (1:10)/10
+crit = read.table('crit.txt',header = TRUE,sep=';',colClasses = c(rep('character',2),rep('numeric',7)))
+
+
+I    = 0.01
 ro   = 1000
 g    = 9.81
 
 
+# loam
+# X = 1.008e+1
+# Y = 5.613e-1
+# v_crit   = 1.079e+1
+# tau_crit = 2.48e-1
 
-a = 23*I**0.5
-b = 1.8
-n = 0.01
-A = a/(100*n)
+# a = X*I**Y
+# b = 1.739
+n = seq(0.001,0.1,length=10)
+# n = 0.00001
+N = length(crit$ID)
 
-tau_crit = 22.5
-v_crit   = 0.264
+hcrit_tau = function (tau_crit,ro,g,I) {tau_crit/(ro*g*I)}
+hcrit_v = function (v_crit,b,n,x,y) {(v_crit*(1/(b-1))*100*n)/(x*I**y)}
 
 
-hcrit_tau = tau_crit/(ro*g*I)
 
-hcrit_v   = (v_crit*(1/(b-1))*100*n)/a
 
-ylim = range(hcrit_tau,hcrit_v)
-plot(I,hcrit_v,ylim = ylim)
-lines(I,hcrit_tau)
-
+for (in_ in n){
+  layout(matrix(c(1,2),ncol = 2))
+  hct = c()
+  hcv = c()
+  for (i in 1:N) {
+    hct = c(hct,hcrit_tau(crit$tau[i],ro,g,I))
+    hcv = c(hcv,hcrit_v(crit$v[i],crit$b[i],in_,crit$x[i],crit$y[i]))
+  }
+  
+  barplot(height = hct,names.arg = crit$ID)
+  mtext(paste('manningovo n: ',in_, '; sklon %: ', I*100),side=3,line=2)
+  barplot(height = hcv,names.arg = crit$ID)
+}
